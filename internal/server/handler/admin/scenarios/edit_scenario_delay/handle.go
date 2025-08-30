ae21402873a1919d1cc2c558bb1813c2e35a116a
Maksim Necheporuk <mnecheporuka@ozon.ru>
2025-08-30 11:36:16 +0300

    edit scenario delay

--- internal/server/handler/admin/scenarios/edit_scenario_delay/handle.go
+++ internal/server/handler/admin/scenarios/edit_scenario_delay/handle.go
@@ -1,8 +1,11 @@
 package edit_scenario_delay
 
 import (
+	"encoding/json"
+	"github.com/go-chi/chi/v5"
 	"github.com/it-chep/my_optium_bot.git/internal/module/admin"
 	"net/http"
+	"strconv"
 )
 
 type Handler struct {
@@ -17,6 +20,30 @@ func NewHandler(adminModule *admin.Module) *Handler {
 
 func (h *Handler) Handle() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		_, _ = w.Write([]byte("Test"))
+		ctx := r.Context()
+
+		scenarioIDStr := chi.URLParam(r, "scenario_id")
+		scenarioID, err := strconv.ParseInt(scenarioIDStr, 10, 64)
+		if err != nil {
+			http.Error(w, "invalid step ID", http.StatusBadRequest)
+			return
+		}
+
+		if r.Method != http.MethodPost {
+			http.Error(w, "", http.StatusMethodNotAllowed)
+			return
+		}
+
+		var req Request
+		if err = json.NewDecoder(r.Body).Decode(&req); err != nil {
+			http.Error(w, "failed to decode request: "+err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		err = h.adminModule.Actions.EditScenarioDelay.Do(ctx, scenarioID, req.Delay)
+		if err != nil {
+			http.Error(w, "failed to update scenario delay: "+err.Error(), http.StatusInternalServerError)
+			return
+		}
 	}
 }
--- internal/server/handler/admin/scenarios/edit_scenario_delay/request.go
+++ internal/server/handler/admin/scenarios/edit_scenario_delay/request.go
@@ -1,1 +1,5 @@
 package edit_scenario_delay
+
+type Request struct {
+	Delay string `json:"delay"`
+}
